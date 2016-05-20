cacb14deae904a650c48234f0545e91ec6d04d75
Lizard07 <Lizard.EOS@t-online.de>
2016-05-20 16:05:00 +0200

    set checkoscamrun to 1 as default

--- logstatus.sh
+++ logstatus.sh
@@ -2,7 +2,7 @@
 # created by Lizard
 SERIALFILE="/usr/lib/enigma2/python/Plugins/Extensions/EMMLog/serial"
 TIMEOUTFILE="/usr/lib/enigma2/python/Plugins/Extensions/EMMLog/timeout"
-checkoscamrun=0
+checkoscamrun=1
 if [[ $checkoscamrun == 1 ]]; then
 	oscamversion=$(find -L /tmp -name "oscam.version" 2>/dev/null)
 	oscampid=$(grep "PID:" $oscamversion|awk '{ print $2 }')
